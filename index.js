0d5aa0b8db56b437afee5cb139d2b64afb8b558d
Michael Stevens <codemasterflash@Michaels-MacBook-Pro.local>
2020-01-16 12:07:50 -0600

    made the port dynamic

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require('./api/server.js');
 
-const port = 6000;
+const port = process.env.PORT || 6000;
+
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
